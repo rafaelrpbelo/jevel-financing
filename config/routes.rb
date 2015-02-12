3776bc0a3d3556bf2b25557dde3ef9f934a90344
Rafael Belo <rafaelrpbelo@gmail.com>
2015-02-12 00:21:10 -0300

    WIP: Fazendo simulação

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'financing_tables#index'
 
   resources :financing_tables, except: [ :show ]
+  get '/financing_tables/simulation', to: 'financing_tables#simulation', as: 'simulation_financing_tables'
 end
